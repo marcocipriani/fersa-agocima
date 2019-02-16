b63091d88f78e9dfff7cfe9d9c9c8e881b52e19c
marcocipriani <mcipriani@ymail.com>
2019-02-16 19:53:16 +0100

    nuovo database e conseguente modifica nella query

--- src/dao/UsrDAO.java
+++ src/dao/UsrDAO.java
@@ -11,7 +11,7 @@ import java.sql.*;
 
 public class UsrDAO {
 
-    private static final String SEARCH_QUERY = "select * from \"Usr2\" where \"nickname\" = ? and \"pwd\" = ?";
+    private static final String SEARCH_QUERY = "select * from \"Usr\" where \"nickname\" = ? and \"pwd\" = ?";
     private static final String BAD_QUERY = "select * from ( select * from \"Usr\" where \"reported\" = false ) where \"nickname\" = ? and where \"pwd\" = ?";
 
     private static Connection conn = null;
