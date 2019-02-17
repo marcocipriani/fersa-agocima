c7cc1034b30b39dd10ba4fb0655df9ffc4b8cdf9
Shuriken933 <46653299+Shuriken933@users.noreply.github.com>
2019-02-17 02:15:09 +0100

    Add files via upload

---
+++ src/web/js/scripts.js
@@ -0,0 +1,6 @@
+/*action buttonLogin onClick*/
+/*function loginFunction() {
+  nickname = document.getElementById("inputNickname").value;
+  password = document.getElementById("inputPassword").value;
+  document.getElementById("test").innerHTML = "controllato";
+}*/
