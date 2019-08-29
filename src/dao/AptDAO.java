571295d7fead2c3edaf059755e7746f9fb98209d
Shuriken933 <46653299+Shuriken933@users.noreply.github.com>
2019-08-29 10:57:13 +0200

    Add files via upload

--- src/dao/AptDAO.java
+++ src/dao/AptDAO.java
@@ -11,13 +11,37 @@ import java.util.Vector;
 
 public class AptDAO {
 
-    private static final String SEARCH_ADDRESS_QUERY = "select * from \"Apt\" where \"address\" = ?";
-    private static final String SEARCH_OWNER_QUERY = "select * from \"Apt\" where \"owner\" = ?";
+    private static final String SEARCH_ID_QUERY = "select * from Apt where \"id\" = ?";
+    private static final String SEARCH_ADDRESS_QUERY = "select * from Apt where \"address\" = ?";
+    private static final String SEARCH_OWNER_QUERY = "select * from Apt where \"owner\" = ?";
 
     private static Connection conn = null;
     private static PreparedStatement stmt = null;
 
-    public static Vector<Apt> findApt(String address) {
+    public static Apt findByID(Integer id) {
+
+        Apt a = null;
+
+        try {
+            conn = ConnectTools.getConnection();
+            stmt = conn.prepareStatement(SEARCH_ID_QUERY);
+            stmt.setInt(1, id);
+            stmt.execute();
+            ResultSet rs = stmt.getResultSet();
+
+            rs.first();
+            int aptId = rs.getInt("id");
+            String owner = rs.getString("owner");
+            String addr = rs.getString("address");
+            a = new Apt(aptId, owner, addr);
+
+        } catch (Exception e) { e.printStackTrace(); }
+        finally { ConnectTools.closeConnection(stmt, conn); }
+
+        return a;
+    }
+
+    public static Vector<Apt> findByAddress(String address) {
 
         Vector<Apt> results = new Vector<Apt>();
         Apt a = null;
@@ -44,7 +68,7 @@ public class AptDAO {
         return results;
     }
 
-    public static Vector<Apt> findOwner(String nickname) {
+    public static Vector<Apt> findByOwner(String nickname) {
 
         Vector<Apt> results = new Vector<Apt>();
         Apt a = null;
