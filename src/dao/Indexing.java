df693bbcba58747f2e212a40fc41d3320b6fbbf5
Marco Cipriani <marcocipriani@users.noreply.github.com>
2019-09-05 00:33:45 +0200

    Ripristinato file originale

--- src/dao/Indexing.java
+++ src/dao/Indexing.java
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-package dao;
-
-import java.sql.*;
-
-public class Indexing {
-
-    private static Integer index;
-    private static Connection conn = null;
-    private static Statement stmt = null;
-
-    public static Integer askForIndex(String table){
-
-        try {
-            conn = ConnectTools.getConnection();
-            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-                    ResultSet.CONCUR_READ_ONLY);
-            String sql = "select * from " + '\"' + table + '\"' + " order by id desc LIMIT 1";
-            System.out.println(sql);
-            ResultSet rs = stmt.executeQuery(sql);
-
-            if (!rs.first()) // rs not empty
-                return null;
-
-            boolean moreThanOne = rs.first() && rs.next();
-            assert !moreThanOne;
-            rs.first();
-
-            index = rs.getInt("id");
-            index ++;
-
-        } catch (SQLException se) { System.out.println("Errore in SQL askForIndex"); se.printStackTrace(); }
-        finally { ConnectTools.closeConnection(stmt, conn); }
-
-        System.out.println(index);
-        return index;
-    }
-}
-=======
 package dao;
 
 import java.sql.*;
@@ -72,4 +33,3 @@ public class Indexing {
         return index;
     }
 }
->>>>>>> refs/remotes/origin/master
