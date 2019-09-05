535075864a42c2686705a78b8d8941b7d0521e79
Marco <mcipriani@ymail.com>
2019-09-05 11:25:55 +0200

    Sensibile alla password configurata localmente

--- src/dao/ConnectTools.java
+++ src/dao/ConnectTools.java
@@ -4,6 +4,7 @@
 
 package dao;
 
+import java.io.*;
 import java.sql.*;
 
 public class ConnectTools {
@@ -11,14 +12,16 @@ public class ConnectTools {
     private static final String DRIVER_CLASS_NAME = "org.postgresql.Driver";
     private static final String URL = "jdbc:postgresql://localhost:5432/agocimadb";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "galaxy070592";
+    private static String PASSWORD = getPassword(); // retrieve pwd from pwd.txt in root project directory
 
     private static Connection conn = null;
 
     public static Connection getConnection(){
+        // to log the password used // System.out.println("Password: " + PASSWORD);
         try {
-            Class.forName(DRIVER_CLASS_NAME); // si chiede dinamicamente al ClassLoader di caricare la classe
+            Class.forName(DRIVER_CLASS_NAME); // at runtime asks loading the class to the ClassLoader
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            conn.setAutoCommit(false); // prevent connection from close automatically
 
         }
         catch (Exception e) { e.printStackTrace(); }
@@ -36,4 +39,22 @@ public class ConnectTools {
                 conn.close();
         } catch (SQLException se) { se.printStackTrace(); }
     }
+
+    public static String getPassword(){
+        BufferedReader in = null;
+        String s = null;
+
+        try {
+            in = new BufferedReader( new FileReader("./pwd.txt"));
+        } catch (FileNotFoundException fnfe) { System.out.println("File pwd.txt not found\n"); fnfe.printStackTrace(); }
+
+
+        try {
+            s = in.readLine();
+            in.close();
+        } catch (Exception e) { System.out.println("Error in reading pwd.txt\n"); e.printStackTrace(); }
+
+        return s;
+    }
+
 }
