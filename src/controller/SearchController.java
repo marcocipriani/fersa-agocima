4b02e94d789a7c4065760b15a177d26a97d938c9
Marco <mcipriani@ymail.com>
2019-09-14 13:21:26 +0200

    Nuovi metodi di ricerca con tipo di ritorno Vector

--- src/controller/SearchController.java
+++ src/controller/SearchController.java
@@ -4,41 +4,21 @@ import java.util.Vector;
 
 import dao.EvalAptDAO;
 import dao.EvalUsrDAO;
-import dao.UsrDAO;
-import model.Eval;
-import model.EvalApt;
-import model.EvalUsr;
 
 public class SearchController { 
 
 	private SearchController() {
     }
-	
-	public static Eval[] searchList(boolean choice, String keyword ) {
-        if (choice == false) {
-        	Vector evalList= EvalAptDAO.findEvalMadeByYou(keyword);
-        	EvalApt[] e = new EvalApt[evalList.size()];
-        	evalList.toArray(e);
-        	return e;       	
-        }
-        else {
-        	Vector evalList= EvalUsrDAO.findEvalMadeByYou(keyword);
-        	EvalUsr[] e = new EvalUsr[evalList.size()];
-        	evalList.toArray(e);
-        	return e;       	
+
+	public static Vector searchList(boolean choice, String keyword) {
+        if (!choice) {
+        	Vector aptList = EvalAptDAO.findByAddress(keyword);
+			System.out.println("@SearchController.java: " + aptList.size());
+        	return aptList;
+        } else {
+			Vector usrList = EvalUsrDAO.findEvalAboutYou(keyword);
+			System.out.println("@SearchController.java: " + usrList.size());
+			return usrList;
         }
     }
-
-    
-
-    /**
-     * Carica l'utente corrispondente alla coppia username/password in input
-     *
-     * @param username username
-     * @param password password
-     * @return l'utente loggato oppure null se nessun utente corrisponde alla coppia username/password
-     */
-    
 }
-
-
