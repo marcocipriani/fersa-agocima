90942a03aa16a6b8d68105799dbb34d9e7a3641f
Marco <mcipriani@ymail.com>
2019-09-16 17:25:44 +0200

    Introduzione dell'attributo ausialiare

--- src/bean/ViewBean.java
+++ src/bean/ViewBean.java
@@ -2,6 +2,7 @@ package bean;
 
 public class ViewBean {
 	private int id;
+	private String aux;
 
 	public ViewBean() {
 		this.id = 0; // default value
@@ -10,6 +11,10 @@ public class ViewBean {
 	public int getId() { return id; }
 
 	public void setId(int id) { this.id = id; }
+
+	public String getAux() { return aux; }
+
+	public void setAux(String aux) { this.aux = aux; }
 
 	public boolean view() {
 		if (this.id == 0) { return false; }
