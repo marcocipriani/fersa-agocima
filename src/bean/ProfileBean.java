7070f9eb818de8367841864d08690eab3c56a05f
Marco <mcipriani@ymail.com>
2019-09-18 20:11:45 +0200

    Gestione del firstTime

--- src/bean/ProfileBean.java
+++ src/bean/ProfileBean.java
@@ -1,43 +1,44 @@
 package bean;
 
 public class ProfileBean {
-	private boolean firsTime;
+	private boolean firstTime;
 	private String username;
 	private String password;
 	private Boolean loginRole;
 	
 	public ProfileBean() {
-		
-		this.firsTime = false;
+		this.firstTime = true;
 		this.username = "";
 		this.password = "";
 		this.loginRole = false;
-		System.out.println("creato nel costruttore");
 	}
+
 	public boolean isFirsTime() {
-		return firsTime;
+		return firstTime;
 	}
 	public void setFirsTime(boolean firsTime) {
-		this.firsTime = firsTime;
+		this.firstTime = firsTime;
 	}
+
 	public String getUsername() {
 		return username;
 	}
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Boolean getLoginRole() {
 		return loginRole;
 	}
 	public void setLoginRole(Boolean loginRole) {
 		this.loginRole = loginRole;
 	}
-	
-	
+
 }
