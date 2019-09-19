52b23d5a2cef80add8da4ee5a831fbe820c533d7
Marco <mcipriani@ymail.com>
2019-09-19 05:07:30 +0200

    Aggiunta funzione di cancellazione valutazione

--- src/controller/EditController.java
+++ src/controller/EditController.java
@@ -7,19 +7,19 @@ import dao.EvalUsrDAO;
 import model.Eval;
 
 public class EditController {
-    public static void setEval(int evalId,String text,int stars,boolean isForUsr) throws SQLException {
-        if (!isForUsr){ 
-        	EvalAptDAO.updateEval(text, stars, evalId);
-        } else { 
-        	EvalUsrDAO.updateEval(text, stars, evalId);
-        	}
+    public static void setEval(int evalId, String text, int stars, boolean isForUsr) throws SQLException {
+        if (!isForUsr){ EvalAptDAO.updateEval(text, stars, evalId); }
+        else { EvalUsrDAO.updateEval(text, stars, evalId); }
     }
     
     public static Eval getEval(int evalId, boolean isForUsr){
-        if (!isForUsr){ 
-        	return (EvalAptDAO.findById(evalId));
-        } else { 
-        	return EvalUsrDAO.findById(evalId);}
+        if (!isForUsr){ return (EvalAptDAO.findById(evalId)); }
+        else { return EvalUsrDAO.findById(evalId);}
+    }
+
+    public static void deleteEval(int evalId, boolean isForUsr){
+        if (!isForUsr){ EvalAptDAO.deleteEval(evalId); }
+        else { EvalUsrDAO.deleteEval(evalId); }
     }
 }
 
